445ad101590688700a66716d6dbb104e0f60d898
jonpodtu <57094931+jonpodtu@users.noreply.github.com>
2021-04-30 19:56:35 +0200

    Primarily changes to main script and added requirements.txt

--- ScriptImplementation/main.py
+++ ScriptImplementation/main.py
@@ -23,6 +23,7 @@ from stockfish import Stockfish
 sf = Stockfish(path + "stockfish_13_win_x64")
 book = path + "the-generated-opening-book.bin"
 
+import pandas as pd
 '''
 # Play one nice, simple and relaxing game of chess:
 our_bot = MinimaxAgent(depth=2, stockfish=sf, book=book)
@@ -33,34 +34,39 @@ result = env.play_game()
 '''
 
 # Elo score dependant on depth:
-depth_range = range(3)
-no_games = 6
+depth_range = range(6)
+games_per_bot = 1
+
 scores = []
 times = []
 
-opponent_scores = [500, 1000, 1500, 2000, 2500, 3000]
+opponent_scores = [500, 1000, 1500, 2000, 2500, 3000][:3]
+no_games = len(opponent_scores)
 
-with tqdm(total=len(depth_range)*no_games) as pbar:
+results = pd.DataFrame(columns=['elo', 'depth', 'time', 'opponent_elo'])
+
+with tqdm(total=len(depth_range)*no_games*games_per_bot) as pbar:
     for i in depth_range:
         game_results = []
+        sum_opponent_scores = 0
         for j in range(no_games):
-            our_bot = MinimaxAgent(depth=depth_range[i]+1, stockfish=sf, book=book)
-            opposite_bot = StockfishAgent(elo_score=opponent_scores[j], stockfish=sf)
-            env = ChessEnvironment(good_bot=our_bot, bad_bot=opposite_bot)
-
-            #print('Game', j+1, ' Depth: ',i+1 )
-            game, avg_movetime = env.play_game()
-            if game == '0-1':
-                game_results.append(-1)
-            elif game == '1/2-1/2':
-                game_results.append(0)
-            else:
-                game_results.append(1)
-            pbar.update(1)
-        elo = get_elo(game_results, opponent_scores)
-        scores.append(elo)
-        times.append(avg_movetime)
-print(scores)
-print(times)
+            for k in range(games_per_bot):
+                our_bot = MinimaxAgent(depth=depth_range[i]+1, stockfish=sf, book=book)
+                opposite_bot = StockfishAgent(elo_score=opponent_scores[j], stockfish=sf)
+                env = ChessEnvironment(good_bot=our_bot, bad_bot=opposite_bot)
+                #print('Game', j+1, ' Depth: ',i+1 )
+                game, avg_movetime = env.play_game()
+                if game == '0-1':
+                    game_results.append(-1)
+                elif game == '1/2-1/2':
+                    game_results.append(0)
+                else:
+                    game_results.append(1)
+                pbar.update(1)
+                sum_opponent_scores += opponent_scores[j]
+            elo = get_elo(game_results, opponent_scores[:j+1])
+            results = results.append({'elo': elo, 'depth': int(i+1), 'time': avg_movetime, 'opponent_elo': opponent_scores[j]}, ignore_index=True)
+            print('\n', results)
+results.to_csv('results.csv', index_label='game_number')
 
 # Time for plot
---
+++ ScriptImplementation/plot.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+class plot_functions():
+    def plot(self, x, y):
+        
