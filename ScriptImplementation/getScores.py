93a193fd7e94128c168575a3c7006fc8ebba343e
jonpodtu <57094931+jonpodtu@users.noreply.github.com>
2021-05-04 15:00:29 +0200

    Update getScores.py

--- ScriptImplementation/getScores.py
+++ ScriptImplementation/getScores.py
@@ -1,7 +1,7 @@
 # Define the weighted elo score over a number of games:
 def get_elo(game_results, opponent_scores):
     # opponent_scores = [x for ind, x in enumerate(opponent_scores) if game_results[ind]!='1/2-1/2']
-    opponent_sum = sum(opponent_scores)
+    opponent_sum = opponent_scores
     games = len(game_results)
     wins = game_results.count(1)
     losses = game_results.count(-1)
