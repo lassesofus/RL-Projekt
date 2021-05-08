e70ec34b70320c99f2c28357d2faaa0ccbd426a1
jonpodtu <57094931+jonpodtu@users.noreply.github.com>
2021-05-08 12:39:23 +0200

    Updated Results.pdf amd rl_proj_eval.py

--- rl_proj_eval.py
+++ rl_proj_eval.py
@@ -44,9 +44,9 @@ def process_results_single(dataframes, names): # Set modelconfig to one of the f
     sns.set_theme()
        
        	# Fist plot:
-    fig = plt.figure(figsize=(10,4))
+    fig = plt.figure(figsize=(10,10))
     #fig.suptitle('Performance of the Chessbot ', fontsize=16)
-    ax1 = fig.add_subplot(131)
+    ax1 = fig.add_subplot(221)
        
     color = 'steelblue'
     ax1.set_xlabel('Depth')
@@ -54,21 +54,29 @@ def process_results_single(dataframes, names): # Set modelconfig to one of the f
     ax1.set_ylim(bottom = 1150, top = 1700)
     ax1.tick_params(axis='y')
        
-    ax2 = fig.add_subplot(132)
-       
-    # color = 'tab:blue'
+    ax2 = fig.add_subplot(222)
     ax2.set_xlabel('Depth')
     ax2.set_ylabel('Avg. time per move (s)')
     ax2.tick_params(axis='y')
     ax2.set_xticks(range(1, len(dataframes)+1))
        
-    ax3 = fig.add_subplot(133)
-       	#color = 'tab:blue'
+    ax3 = fig.add_subplot(223)
     ax3.set_xlabel('Depth')
     ax3.set_ylabel('Avg. Outcome')
     ax3.set_ylim(-1, 1)
     ax3.axhline(y=0, c='black')
+    
        
+    boxplot_plot = pd.concat(dataframes)
+    boxplot_plot['Interval'] = (10*['100-1000']+10*['1100-2000']+10*['2100-3000'])*4*4
+    boxplot_plot['Bot'] = 120*[names[0]]+120*[names[1]]+120*[names[2]] + 120*[names[3]]
+    ax4 = fig.add_subplot(224)
+    sns.lineplot(ax = ax4, x='Interval', y="outcome", hue="Bot", data=boxplot_plot, palette=colors, err_style="bars", ci=68)
+    ax4.set_ylabel('Outcome')
+    ax4.get_legend().remove()
+
+    
+    
     total_width = 0.8
     n_bars = 4
     bar_width = total_width / n_bars
@@ -78,20 +86,23 @@ def process_results_single(dataframes, names): # Set modelconfig to one of the f
     for index, plotdata in enumerate(plotdatalist):
         # The offset in x direction of that bar
         x_offset = (index - n_bars / 2) * bar_width + bar_width / 2
-    
         # Draw a bar for every value of that type
         ax1.bar([float(i)+x_offset for i in x_values], plotdata['elo'], width=bar_width * single_width, label = names[index], color=colors[index], alpha = 0.7)
         if index == 1: # To solve to bars on top of each others
             ax2.plot(x_values,plotdata['time'], ls = '--', color = colors[index], alpha = 0.7, lw = 3, label = names[index])
         else:
             ax2.plot(x_values,plotdata['time'], color = colors[index], alpha = 0.7, lw = 3, label = names[index])
-        print(plotdata['time'])
-        ax3.bar([float(i)+x_offset for i in x_values], plotdata['outcome'], width=bar_width * single_width, label = names[index], color = colors[index], alpha = 0.7)
-        plt.legend()
-    # Plot the whole molevit
+        ax3.bar([float(i)+x_offset for i in x_values], plotdata['outcome'], width=bar_width * single_width, label = names[index], color = colors[index], alpha = 0.7)       
+        
+    
+    lines, labels = fig.axes[-1].get_legend_handles_labels()
+    fig.legend(lines, labels, bbox_to_anchor=(0.5, 0), loc = 'lower center', ncol = 4)
+    # Plot the whole molevit                right  up   right 
     plt.tight_layout()
+    fig.subplots_adjust(bottom=0.1)
     plt.savefig("Results.pdf")
     plt.show()
+    
 
 # Runcode: 
 process_results_single(chessdata, namesdata)
